4505d4f22cba6c12e2b5637df22008496a7f48b1
u.palo <u.palo@OPE19406.replynet.prv>
2018-08-03 12:39:10 +0200

    versione funzionante o quasi. usato camel 2.18.0 più recenti non va. connessione ad aws mediante componente avviene o almeno non ci sono eccezioni. caritato messaggio di test su coda di dev dove il consumenr è in ascolto...... ma nell'exchnage con c'è una sega

--- demo/src/main/java/it/umberto/camel/controller/CamelController.java
+++ demo/src/main/java/it/umberto/camel/controller/CamelController.java
@@ -37,16 +37,10 @@ public class CamelController {
 	
 	@RequestMapping(value = "/awsRead")
 	public void startCamelAWSRead() throws Exception {
-		Exchange e =consumerTemplate.receive("aws-sqs://{{umberto.queue.aws.name}}?accessKey={{umberto.queue.aws.accessKey}}&amp;secretKey=RAW({{umberto.queue.aws.secretKey}})&amp;amazonSQSEndpoint={{umberto.queue.aws.amazonSQSEndpoint}}&amp;concurrentConsumers=1&amp;maxMessagesPerPoll=1");
-		LOG.info("Il body presente nel messaggio del Exchange sulla rotta è: {} ",e.getMessage().getBody());		
-//		try {
-//			camelContext.addRoutes(new CamelRoutesAWS());
-//			camelContext.start();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}finally {
-//			camelContext.stop();
-//		}
+//		Exchange e =consumerTemplate.receive("aws-sqs://{{umberto.queue.aws.name}}?accessKey={{umberto.queue.aws.accessKey}}&amp;secretKey=RAW({{umberto.queue.aws.secretKey}})&amp;amazonSQSEndpoint={{umberto.queue.aws.amazonSQSEndpoint}}&amp;concurrentConsumers=1&amp;maxMessagesPerPoll=1");
+		Exchange e =consumerTemplate.receive("aws-sqs://{{umberto.queue.aws.name}}?accessKey={{umberto.queue.aws.accessKey}}&secretKey=RAW({{umberto.queue.aws.secretKey}})&amazonSQSEndpoint={{umberto.queue.aws.amazonSQSEndpoint}}&concurrentConsumers=1&maxMessagesPerPoll=1");
+		LOG.info("Il body presente nel messaggio del Exchange sulla rotta è: {} ",e);		
+
 		
 			
 	}
