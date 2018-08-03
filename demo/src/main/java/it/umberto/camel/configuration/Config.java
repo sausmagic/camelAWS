effa8f1efb7ba9ae81c2832df7447779f1035797
u.palo <u.palo@OPE19406.replynet.prv>
2018-08-03 13:31:10 +0200

    Aggiunto Swagger 2 per documentazione API esposte dalla webapp raggiungile da qui: {HOST}:{PORT}/{CONTEXT}/v2/api-docs
    per come qui configurato:
    
    http://localhost:8090/umbCamel/v2/api-docs

--- demo/src/main/java/it/umberto/camel/configuration/Config.java
+++ demo/src/main/java/it/umberto/camel/configuration/Config.java
@@ -8,8 +8,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
 @PropertySource("classpath:application.properties")
+@EnableSwagger2
 public class Config {
 
 	private static final Logger logger = LoggerFactory.getLogger(Config.class);
@@ -23,6 +30,15 @@ public class Config {
 		logger.info("Bean con file di properties caricato....");
 		return bean;
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 	
 	
 }
